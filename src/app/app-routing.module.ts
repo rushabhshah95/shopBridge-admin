600868320d002a57cc5adf44a3aaa7844e41acde
rushabhshah95 <russhah95@gmail.com>
2021-03-28 16:08:53 +0530

    Code Comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
+/* This is a Route module, where all the Route configuration ofapplication are configured */
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductCreateComponent } from './components/product-create/product-create.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'create', pathMatch: 'full' },
   { path: 'products', component: ProductListComponent },
-  // { path: 'products/:id', component: ProductDetailsComponent },
   { path: 'create', component: ProductCreateComponent },
   {path: '404', component: NotfoundComponent},
   {path: '**', redirectTo: '/404'}
